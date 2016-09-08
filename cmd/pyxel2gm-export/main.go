f834e5963c5e3a8575f2f9bdbd88c136030d1389
Richard Hawkins <hurricanerix@gmail.com>
2016-09-07 22:49:08 -0500

    Add pyxel2gm-editor command

--- cmd/pyxel2gm-export/main.go
+++ cmd/pyxel2gm-export/main.go
@@ -11,44 +11,9 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
+
 package main
 
-import (
-	"flag"
-	"fmt"
-	"os"
-
-	"github.com/hurricanerix/pyxel2gm/app"
-)
-
-var (
-	assetsDir   string
-	projectDir  string
-	showVersion bool
-)
-
-func init() {
-	flag.StringVar(&assetsDir, "assets-dir", "assets", "directory to scan for .pyxel files")
-	flag.StringVar(&projectDir, "project-dir", ".", "Game Maker project directory to export into")
-	flag.BoolVar(&showVersion, "version", false, "display the version of this app")
-}
-
 func main() {
-	flag.Parse()
-
-	if showVersion {
-		fmt.Println(app.Version)
-		os.Exit(0)
-	}
-
-	// Create app context
-	a := app.Context{
-		AssetsDir:  assetsDir,
-		ProjectDir: projectDir,
-	}
-
-	// Run app
-	if err := a.Run(); err != nil {
-		fmt.Fprintf(os.Stderr, "%s", err)
-	}
+	println("TODO: write this!")
 }
